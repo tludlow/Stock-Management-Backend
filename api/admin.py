ed4a3925af4f73046be0338055ce2dba28b343fc
David Golding <43792102+davidjgolding@users.noreply.github.com>
2020-01-28 16:26:56 +0000

    base api functionality

---
+++ api/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from .models import *
+
+admin.site.register(Company)
+# admin.site.register(Product)
+# admin.site.register(ProductSeller)
+# admin.site.register(ProductPrice)
+admin.site.register(Currency)
+admin.site.register(CurrencyPrice)
+# admin.site.register(Seller)
+admin.site.register(StockPrice)
+admin.site.register(Trade)
