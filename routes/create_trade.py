48d7cfcaa35a890483769461d10972f52f9799c4
tludlow99 <twjludlow@gmail.com>
2020-01-30 19:17:42 +0000

    Removed the code from the example backend, switched frameworks since then.

--- routes/create_trade.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint, request
-
-create_trade = Blueprint("create_trade", __name__)
-
-@create_trade.route('/trade', methods=['POST'])
-def deleteTrade():
-    print(request.form)
-    return {"currency": request.form['currency']}
-    
