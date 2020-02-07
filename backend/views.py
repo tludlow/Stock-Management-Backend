f8876115e7ea6aa9a5728f88c8ec080626005f60
David Golding <43792102+davidjgolding@users.noreply.github.com>
2020-02-07 23:56:04 +0000

    introduced require post

--- backend/views.py
+++ backend/views.py
@@ -5,6 +5,7 @@ from django.conf import settings
 from django.http import HttpResponse, HttpResponseForbidden, HttpResponseServerError
 from django.views.decorators.csrf import csrf_exempt
 from django.utils.encoding import force_bytes
+from django.views.decorators.http import require_POST
 
 import os
 
