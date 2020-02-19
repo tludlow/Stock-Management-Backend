b71e10ffc945dfcd2db7493f8e1ca3db7346dea4
tludlow99 <twjludlow@gmail.com>
2020-02-19 16:23:51 +0000

    Modified the recent trade list to include the company name within each trade. Optimised this route so it only selects information required for the current page

--- api/views/specific.py
+++ api/views/specific.py
@@ -6,6 +6,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from django.http import HttpResponse, JsonResponse
 from api.serializers import *
+from django.core import serializers
 from ..models import *
 from ..serializers import *
 import datetime
@@ -16,12 +17,30 @@ import random, string
 
 class TradeRecentList(APIView):
     def get(self, request):
-        data = Trade.objects.all().order_by('-date')
+        #Get pagination data before the request so that it saves memory and is quicker to query.
+        page_number = int(self.request.query_params.get("page_number", 1))
+        page_size = int(self.request.query_params.get("page_size", 12))
 
-        #Pagination 
-        page_number = self.request.query_params.get("page_number", 1)
-        page_size = self.request.query_params.get("page_size", 12)
-        paginator = Paginator(data, page_size)
+        data = Trade.objects.all().order_by('-date')[(page_number-1)*page_size : page_number*page_size]
+        trade_data = data.values()
         
-        s = TradeSerializer(paginator.page(page_number), many=True)
-        return Response(s.data)
+        for idx, trade in enumerate(trade_data):
+            buying_party_id = trade.get("buying_party_id")
+            selling_party_id = trade.get("selling_party_id")
+
+            #Get the data for the buying and selling party
+            #Needs to be done here, cannot be done in the original call recursively because its too slow.
+            buying_party_company_data = Company.objects.get(id=buying_party_id)
+            buying_company = CompanySerializer(buying_party_company_data)
+
+            selling_party_company_data = Company.objects.get(id=selling_party_id)
+            selling_company = CompanySerializer(selling_party_company_data)
+
+            #Add the companies data to the trade
+            trade_data[idx]["buying_company"] = buying_company.data.get("name")
+            trade_data[idx]["selling_company"] = selling_company.data.get("name")
+
+            #print(str(idx) + ":  " + str(trade), end="\n\n")
+
+        #Modified the structure of a trade, will need to use a custom serializer.
+        return Response(trade_data)
